6304d8da3440438c2fc32078cba1d360b36aad53
priscillamc <priscillamc@users.noreply.github.com>
2018-12-26 12:49:26 -0500

    Adds ability to change the Jetpack portfolio title and description for display in archive pages.

--- pm-enhanced-portfolio.php
+++ pm-enhanced-portfolio.php
@@ -16,24 +16,35 @@
  * Plugin URI:  https://priscillachapman.com/
  * Description: Adds enhanced features and allows you to customize Jetpack Portfolio features
  * Version:     1.0.0
- * Author:      Priscilla Chapman (priscillamc)
+ * Author:      Priscilla Chapman
  * Author URI:  https://priscillachapman.com/
  * Text Domain: pm-enhanced-portfolio
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * BuildDate: 20181226
  */
 	 
 add_action( 'admin_menu', 'pmep_add_admin_menu' );
 add_action( 'admin_init', 'pmep_settings_init' );
+add_action( 'registered_post_type', 'pmep_change_post_type', 10, 2 );
 
-function pmep_add_admin_menu(  ) { 
+/**
+ * Adds the menu item to Settings
+ * 
+ * @since 1.0
+ */
+function pmep_add_admin_menu() { 
 
 	add_submenu_page( 'options-general.php', 'PM Enhanced Portfolio', 'PM Enhanced Portfolio', 'manage_options', 'pm_enhanced_portfolio', 'pmep_options_page' );
 
 }
 
-
-function pmep_settings_init(  ) { 
+/**
+ * Registers the settings fields
+ * 
+ * @since 1.0
+ */
+function pmep_settings_init() { 
 
 	register_setting( 'pmep_page', 'pmep_settings' );
 
@@ -64,54 +75,58 @@ function pmep_settings_init(  ) {
 }
 
 /**
- * [[Description]]
+ * Display the title field
  * 
- * @todo Add default title if not set
+ * @since 1.0
  */
-function pmep_portfolio_title_render(  ) { 
+function pmep_portfolio_title_render() { 
 
 	$options = get_option( 'pmep_settings' );
+	$default = get_post_type_object('jetpack-portfolio')->labels->name;
+	$title = ( isset( $options['portfolio_title'] ) )? $options['portfolio_title'] : $default;
 	?>
-	<input type="text" name="pmep_settings[portfolio_title]" value="<?php echo $options['portfolio_title']; ?>">
+	<input type="text" name="pmep_settings[portfolio_title]" value="<?php echo $title; ?>">
 	<?php
 
 }
 
 /**
- * [[Description]]
+ * Display the description field
  * 
- * @todo Add the default description if not set
- * @todo Hook into WordPress text editor
+ * @since 1.0
  */
 function pmep_portfolio_description_render(  ) { 
 
 	$options = get_option( 'pmep_settings' );
+	$default = get_post_type_object('jetpack-portfolio')->description;
+	$description = ( isset( $options['portfolio_description'] ) )? $options['portfolio_description'] : $default;
 	?>
-	<textarea cols="40" rows="5" name="pmep_settings[portfolio_description]"><?php echo $options['portfolio_description']; ?></textarea>
+	<textarea cols="40" rows="5" name="pmep_settings[portfolio_description]"><?php echo $description; ?></textarea>
 	<?php
 
 }
 
 /**
- * [[Description]]
+ * Display a settings message
+ * 
+ * @since 1.0
  */
 function pmep_settings_section_callback(  ) { 
-
-	echo __( '', 'pm-enhanced-portfolio' );
 
 }
 
 /**
- * [[Description]]
+ * Display the options page
+ * 
+ * @since 1.0
  */
-function pmep_options_page(  ) { 
+function pmep_options_page() { 
 
 	?>
 	<form action="options.php" method="post">
 
 		<h2>PM Enhanced Portfolio</h2>
-		<p><?php echo __( '', 'pm-enhanced-portfolio' ); ?></p>
-
+		
 		<?php
 		settings_fields( 'pmep_page' );
 		do_settings_sections( 'pmep_page' );
@@ -120,6 +135,27 @@ function pmep_options_page(  ) {
 		
 	</form>
 	<?php
+}
+
+/**
+ * Changes the Jetpack Portfolio labels
+ * 
+ * @param string $post_type        
+ * @param WP_Post_Type $post_type_object 
+ * @since 1.0
+ */
+function pmep_change_post_type( $post_type, $post_type_object ){
 	$options = get_option( 'pmep_settings' );
-	var_dump($options);
+	
+	if ( 'jetpack-portfolio' == $post_type ){
+		// Replace the title
+		if ( isset( $options['portfolio_title'] ) ){
+			$post_type_object->labels->name = $options['portfolio_title'];
+		}
+
+		// Replace the description
+		if ( isset( $options['portfolio_description'] ) ){
+			$post_type_object->description = $options['portfolio_description'];
+		}	
+	}
 }
